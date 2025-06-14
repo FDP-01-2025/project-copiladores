0aa50b36feffc52d3464893b078108a4d814c5a4
Angel4091 <00048825@uca.edu.sv>
2025-06-14 15:19:38 -0600

    1commit

---
+++ intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string usuario1, usuario2;
+
+    cout << "Welcome to the police and thief racing game" << endl;
+    cout << "        ______________________ " << endl;
+    cout << "        |          |          |  " << endl;
+    cout << "        |          |          |" << endl;
+    cout << "  ________________________________________" << endl;
+    cout << "|                                         |" << endl;
+    cout << "|                                         | " << endl;
+    cout << "  ________________________________________ " << endl;
+    cout << "       |       |            |       |" << endl;
+    cout << "       |       |            |       |" << endl;
+    cout << "       ---------            ---------" << endl;
+    return 0;
+}
