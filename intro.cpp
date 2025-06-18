a8a797105a39c2b494defcbf52b6cd70b87ec88d
Angel4091 <00048825@uca.edu.sv>
2025-06-18 15:11:04 -0600

    2commit

--- intro.cpp
+++ intro.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(void){
     string usuario1, usuario2;
+    int opcion;
+
 
     cout << "Welcome to the police and thief racing game" << endl;
     cout << "        ______________________ " << endl;
@@ -15,5 +17,36 @@ int main(void){
     cout << "       |       |            |       |" << endl;
     cout << "       |       |            |       |" << endl;
     cout << "       ---------            ---------" << endl;
+
+
+    cout << "Elije el nivel de dificultad del juego" << endl;
+    cout << " 1. Facil" << endl;
+    cout << " 2. Medio" << endl;
+    cout << " 3. Dificil" << endl;
+    cout << " Si deseas salir selecciona la siguiente opcion" << endl;
+    cout << " 4. Salir" << endl;
+    cout << "\n";
+    cin >> opcion;
+    switch (opcion)
+    {
+    case 1:
+        cout << "Elijiste la opcion facil" << endl;
+        cout << "Cargando..." << endl;
+        break;
+    case 2:
+        cout << "Elijiste la opcion medio" << endl;
+        cout << "Cargando..." << endl;
+        break;
+    case 3:
+        cout << "Elijiste la opcion dificil" << endl;
+        cout << "Cargando..." << endl;
+        break;
+    case 4:
+        cout << "Saliendo..." << endl;
+        break;
+    default:
+        cout << "Elije una opcion entre 1 y 4" << endl;
+        break;
+    }
     return 0;
 }
