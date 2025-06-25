be51ef64580259d56b013757cfbc673567c01bcb
Moran-14 <00133225@uca.edu.sv>
2025-06-25 05:59:45 -0600

    Actualizacion de tablero

--- Tablero.cpp
+++ Tablero.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 #include "Dado.h"
 #include "personaje.h"
+#include "mostrar_tablero.h"
 using namespace std;
 
-void mostrar_tablero(int M[][10], int nfilas, int ncolumnas){
-    cout << "=== Tablero ===\n";
-    for(int i = 0; i < nfilas; i++){
-        cout << "[ ";
-        for(int j = 0; j < ncolumnas; j++){
-            cout << M[i][j] << " ";
-        }
-        cout << "]\n";
-    }
-    cout << "\n";
+bool juego_finalizado(){
+    
 }
 
-void avanzar(int M[][10]){
+void avanzar(string M[][6]){
     int dado;
     int n = 0;
     int casilla_global = 0;
     dado = lanzar_dado(dado, n);
     int fila, columna;
-    for(int pos = 0; pos < dado; pos++){
-        fila = pos / 10;
-        columna = pos % 10;
-        casilla_global += dado;
+    casilla_global += dado;
+    for(int i = 0; i < dado; i++){
+        fila = i / 10;
+        columna = i % 10;
         cout << "Usted está en la casilla: " << casilla_global << "\n";
         /*TODO: Agregar que la funcion del juegador, 
         debe avanzar conforme sea el resultado de lanzar_dado en el tablero*/
@@ -32,10 +25,13 @@ void avanzar(int M[][10]){
 }   
 
 int main(void){
-    int tablero[2][10] = { {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {11, 12, 13, 14, 15, 16, 17, 18, 19, 20} };
+    string tablero[9][6] = { {"1  ", "2  ", "3  ", "4  ", "5  ", "6  "}, {"20 ", "   ", "   ", "   ", "   ", "7  "}, {"19 ", "   ", "   ", "   ", "   ", "8  "}, {"18 ", "   ", "   ", "   ", "   ", "9  "}, {"17 ", "   ", "   ", "   ", "   ", "10 "}, {"16", "15 ", "14 ", "13 ", " 12 ", "11 "} };
+    bool juego_finalizado = false;
 
-    mostrar_tablero(tablero, 2, 10);
-    avanzar(tablero);
-
+    do{
+        mostrar_tablero(tablero, 2, 10);
+        avanzar(tablero);
+    }while(!juego_finalizado);
+    
     return 0;
 }
