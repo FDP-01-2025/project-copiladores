3d7211d6511d4692b6157f5f5d222acd881345b9
Luis Jose Murcia Portillo <00249025@uca.edu.sv>
2025-06-30 19:05:34 -0600

    Header.h v3

--- Header.h
+++ Header.h
@@ -8,4 +8,6 @@ void mostrar_tablero(string M[][6], int nfilas, int ncolumnas);
 int lanzar_dado();
 void mostrarDado(int num);
 void avanzar(string M[][6]);
-void jugador_avanza(std::string M[][6], int &pos_jugador, int dado, int nfilas, int ncolumnas);
+void casilla_a_coordenadas(int casilla, int &fila, int &col);
+void jugador_avanza(string M[][6], int &pos_jugador, int dado, int nfilas, int ncolumnas);
+bool juego_finalizado(int pos_jugador);
